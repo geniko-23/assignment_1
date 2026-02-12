123678cf833607cc8a93116f34be5ccdf4fa93ff
Georg Dahl-Steinsvik <gedaa002@outlook.com>
2026-02-12 23:32:45 +0100

    working on task 3_2

---
+++ task_3_2/task_3_2.sh
@@ -0,0 +1,11 @@
+# Cloning the GitHub code
+cd ~/NMBU_2026-2026/TEL211/assignment_1/task_3_2
+git clone https://github.com/NMBURobotics/TEL211_2023.git
+
+# Hard code the names off the diractory in the path
+cd ~/NMBU_2026-2026/TEL211/assignment_1/task_3_2/TEL211_2023/assignment_1_snippets
+
+# Now i want to run every .sh script in the diractory i cloned
+
+for script in *.sh
+
